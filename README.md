65eeb18836b29515bf06c08ac352dfe9e7cfcb0e
florane <qwerty.qa912@gmail.com>
2026-05-14 10:51:19 +0700

    proper CSR support + autoupdate file

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Replaces the behavior of ModifierLessPagers - instead of decreasing the amount of pagers, it decreases time within which pager can be answered
 Extra Heist Info support changes the pager tracker to reflect actual time of the unanswered pager.
+Crime Spree Roguelike is also supported
--- modifierlesstime.lua
+++ modifierlesstime.lua
@@ -1,20 +1,22 @@
 if ModifierCSRLessPagers1 then
+	--CSR implementation
 	local function init(self, data)
 		self._data = data
 		self.icon = "crime_spree_pager"
 
-		tweak_data.player.alarm_pager.call_duration[1][1] = 6 - self:value()
-		tweak_data.player.alarm_pager.call_duration[2][1] = 6 - self:value()
-		tweak_data.player.alarm_pager.call_duration[1][2] = 6 - self:value()
-		tweak_data.player.alarm_pager.call_duration[2][2] = 6 - self:value()
+		tweak_data.player.alarm_pager.call_duration[1][1] = 6 - data.count*2
+		tweak_data.player.alarm_pager.call_duration[2][1] = 6 - data.count*2
+		tweak_data.player.alarm_pager.call_duration[1][2] = 6 - data.count*2
+		tweak_data.player.alarm_pager.call_duration[2][2] = 6 - data.count*2
 
 		if EHIPagerTracker then
-			EHIPagerTracker._forced_time = 12 - (self:value()*2)
+			EHIPagerTracker._forced_time = 12 - (data.count*2)
 		end
 		if EHIPagerWaypoint then
-			EHIPagerWaypoint._forced_time = 12 - (self:value()*2)
+			EHIPagerWaypoint._forced_time = 12 - (data.count*2)
 		end
 	end
+	--awesome code, did chatgpt write it for you?
 	ModifierCSRLessPagers1.init = init
 	ModifierCSRLessPagers2.init = init
 	ModifierCSRLessPagers3.init = init
@@ -25,6 +27,7 @@ if ModifierCSRLessPagers1 then
 	ModifierCSRLessPagers3.desc_id = "menu_cs_modifier_pagers"
 	ModifierCSRLessPagers4.desc_id = "menu_cs_modifier_pagers"
 else
+	--General implementation
 	Hooks:OverrideFunction(ModifierLessPagers, "init", function (self, data)
 		ModifierLessPagers.super.init(self, data)
 		tweak_data.player.alarm_pager.call_duration[1][1] = 6 - self:value()
@@ -32,6 +35,7 @@ else
 		tweak_data.player.alarm_pager.call_duration[1][2] = 6 - self:value()
 		tweak_data.player.alarm_pager.call_duration[2][2] = 6 - self:value()
 
+		--i am forever thankful for this
 		if EHIPagerTracker then
 			EHIPagerTracker._forced_time = 12 - (self:value()*2)
 		end
