c436bd4a7c112f42d528761a3b4ff58345979b61
florane <qwerty.qa912@gmail.com>
2026-05-14 09:36:17 +0700

    experimental support for Crime Spree Roguelike

--- modifierlesstime.lua
+++ modifierlesstime.lua
@@ -1,14 +1,42 @@
-Hooks:OverrideFunction(ModifierLessPagers, "init", function (self, data)
-    ModifierLessPagers.super.init(self, data)
-    tweak_data.player.alarm_pager.call_duration[1][1] = 6 - self:value()
-    tweak_data.player.alarm_pager.call_duration[2][1] = 6 - self:value()
-    tweak_data.player.alarm_pager.call_duration[1][2] = 6 - self:value()
-    tweak_data.player.alarm_pager.call_duration[2][2] = 6 - self:value()
+if ModifierCSRLessPagers1 then
+	local function init(self, data)
+		self._data = data
+		self.icon = "crime_spree_pager"
 
-    if EHIPagerTracker then
-        EHIPagerTracker._forced_time = 12 - (self:value()*2)
-    end
-    if EHIPagerWaypoint then
-        EHIPagerWaypoint._forced_time = 12 - (self:value()*2)
-    end
-end)
+		tweak_data.player.alarm_pager.call_duration[1][1] = 6 - self:value()
+		tweak_data.player.alarm_pager.call_duration[2][1] = 6 - self:value()
+		tweak_data.player.alarm_pager.call_duration[1][2] = 6 - self:value()
+		tweak_data.player.alarm_pager.call_duration[2][2] = 6 - self:value()
+
+		if EHIPagerTracker then
+			EHIPagerTracker._forced_time = 12 - (self:value()*2)
+		end
+		if EHIPagerWaypoint then
+			EHIPagerWaypoint._forced_time = 12 - (self:value()*2)
+		end
+	end
+	ModifierCSRLessPagers1.init = init
+	ModifierCSRLessPagers2.init = init
+	ModifierCSRLessPagers3.init = init
+	ModifierCSRLessPagers4.init = init
+
+	ModifierCSRLessPagers1.desc_id = "menu_cs_modifier_pagers"
+	ModifierCSRLessPagers2.desc_id = "menu_cs_modifier_pagers"
+	ModifierCSRLessPagers3.desc_id = "menu_cs_modifier_pagers"
+	ModifierCSRLessPagers4.desc_id = "menu_cs_modifier_pagers"
+else
+	Hooks:OverrideFunction(ModifierLessPagers, "init", function (self, data)
+		ModifierLessPagers.super.init(self, data)
+		tweak_data.player.alarm_pager.call_duration[1][1] = 6 - self:value()
+		tweak_data.player.alarm_pager.call_duration[2][1] = 6 - self:value()
+		tweak_data.player.alarm_pager.call_duration[1][2] = 6 - self:value()
+		tweak_data.player.alarm_pager.call_duration[2][2] = 6 - self:value()
+
+		if EHIPagerTracker then
+			EHIPagerTracker._forced_time = 12 - (self:value()*2)
+		end
+		if EHIPagerWaypoint then
+			EHIPagerWaypoint._forced_time = 12 - (self:value()*2)
+		end
+	end)
+end
