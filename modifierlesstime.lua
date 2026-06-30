476db057d4de0d66bc90658a2af770a91be83a45
florane <qwerty.qa912@gmail.com>
2026-06-30 22:20:35 +0700

    removed csr compatability and cleaned up code
    csr went through a rewrite and now implements this mod natively.
    also switched text wrapper to a hook for better mod compat and cleaned up actual code.

--- modifierlesstime.lua
+++ modifierlesstime.lua
@@ -1,46 +1,13 @@
-if ModifierCSRLessPagers1 then
-	--CSR implementation
-	local function init(self, data)
-		self._data = data
-		self.icon = "crime_spree_pager"
+Hooks:OverrideFunction(ModifierLessPagers, "init", function (self, data)
+	ModifierLessPagers.super.init(self, data)
+	local time = 6 - self:value()
+	tweak_data.player.alarm_pager.call_duration = {{time,time},{time,time}}
 
-		tweak_data.player.alarm_pager.call_duration[1][1] = 6 - data.count
-		tweak_data.player.alarm_pager.call_duration[2][1] = 6 - data.count
-		tweak_data.player.alarm_pager.call_duration[1][2] = 6 - data.count
-		tweak_data.player.alarm_pager.call_duration[2][2] = 6 - data.count
-
-		if EHIPagerTracker then
-			EHIPagerTracker._forced_time = 12 - (data.count*2)
-		end
-		if EHIPagerWaypoint then
-			EHIPagerWaypoint._forced_time = 12 - (data.count*2)
-		end
+	--i am forever thankful for this
+	if EHIPagerTracker then
+		EHIPagerTracker._forced_time = time*2
 	end
-	--awesome code, did chatgpt write it for you?
-	ModifierCSRLessPagers1.init = init
-	ModifierCSRLessPagers2.init = init
-	ModifierCSRLessPagers3.init = init
-	ModifierCSRLessPagers4.init = init
-
-	ModifierCSRLessPagers1.desc_id = "menu_cs_modifier_pagers"
-	ModifierCSRLessPagers2.desc_id = "menu_cs_modifier_pagers"
-	ModifierCSRLessPagers3.desc_id = "menu_cs_modifier_pagers"
-	ModifierCSRLessPagers4.desc_id = "menu_cs_modifier_pagers"
-else
-	--General implementation
-	Hooks:OverrideFunction(ModifierLessPagers, "init", function (self, data)
-		ModifierLessPagers.super.init(self, data)
-		tweak_data.player.alarm_pager.call_duration[1][1] = 6 - self:value()
-		tweak_data.player.alarm_pager.call_duration[2][1] = 6 - self:value()
-		tweak_data.player.alarm_pager.call_duration[1][2] = 6 - self:value()
-		tweak_data.player.alarm_pager.call_duration[2][2] = 6 - self:value()
-
-		--i am forever thankful for this
-		if EHIPagerTracker then
-			EHIPagerTracker._forced_time = 12 - (self:value()*2)
-		end
-		if EHIPagerWaypoint then
-			EHIPagerWaypoint._forced_time = 12 - (self:value()*2)
-		end
-	end)
-end
+	if EHIPagerWaypoint then
+		EHIPagerWaypoint._forced_time = time*2
+	end
+end)
--- modifierrename.lua
+++ modifierrename.lua
@@ -1,14 +1,6 @@
-local text_original = LocalizationManager.text
-local testAllStrings = false
-
-function LocalizationManager:text(string_id, ...)
-    local paste_str = ""
-    if string_id == "menu_cs_modifier_pagers" then
-        local arg = ...
-        paste_str = string.format("Decrease pager response time by %d seconds",arg.count*2)
-    end
-    return string_id == "menu_cs_modifier_pagers" and paste_str
-
-    or testAllStrings == true and string_id
-    or text_original(self, string_id, ...)
-end
+Hooks:PostHook(LocalizationManager, "text", "text_less_pagers", function(self, string_id, arg)
+	if string_id == "menu_cs_modifier_pagers" then
+		return string.format("Decrease pager response time by %d seconds",arg.count*2)
+	end
+	return Hooks:GetReturn()
+end)
